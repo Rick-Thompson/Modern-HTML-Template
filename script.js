664517c01c6cd6ced63538f5c470ceb1dec3b445
Desktop\Rick <rick@rthompson.us>
2021-10-10 19:16:19 -0400

    more complete starting template
    Includes colors and web components

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-// Javascript goes in this file
+window.addEventListener('DOMContentLoaded', function () {
+
+    //add things to do after the page is loaded here
+
+});
+
+// web components
+//nav
+fetch("Components/nav.html")
+    .then(stream => stream.text())
+    .then(text => define(text, "nav-menu"));
+//header
+fetch("Components/header.html")
+    .then(stream => stream.text())
+    .then(text => define(text, "header-component"));//nav
+fetch("Components/footer.html")
+    .then(stream => stream.text())
+    .then(text => define(text, "footer-component"));
+
+
+function define(html, navpart) {
+    class NavComponent extends HTMLElement {
+        set value(value) {
+            this._value = value;
+            this.valueElement.innerText = this._value;
+        }
+        get value() {
+            return this._value;
+        }
+        constructor() {
+            super();
+            this._value = 0;
+            var shadow = this.attachShadow({ mode: 'open' });
+            shadow.innerHTML = html;
+        }
+    }
+    customElements.define(navpart, NavComponent);
+}
+
+
+
+
+
+//everything else goes here
